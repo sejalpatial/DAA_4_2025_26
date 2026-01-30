516e281678af082e7047fd3dbedbe5f4f9f5f0d6
sejalpatial <sejalpatial29@gmail.com>
2026-01-30 12:24:24 +0530

    pushed changes

--- SESSION 11/hamiltonian.cpp
+++ SESSION 11/hamiltonian.cpp
@@ -1,39 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n = 5;
-vector<vector<int>> adj = {
-    {1, 3},
-    {0, 2, 3, 4},
-    {1, 4},
-    {0, 1, 4},
-    {1, 2, 3}
+int n=5;
+vector<vector<int>>adj = {
+    {1,3},
+    {0,2,3,4},
+    {1,4},
+    {0,1,4},
+    {1,2,3}
 };
 
-vector<bool> visited(n, false);
+vector<bool> visited(n,false);
 vector<int> path;
 
 bool hasEdge(int u, int v) {
-    for (int x : adj[u]) {
+    for (int x:adj[u]) {
         if (x == v) return true;
     }
     return false;
 }
 
-bool solve(int u, int count) {
-    if (count == n) {
-        return hasEdge(u, path[0]); 
+bool solve(int u,int cnt) {
+    if (cnt==n) {
+        return hasEdge(u,path[0]); 
     }
 
-    for (int v : adj[u]) {
+    for (int v:adj[u]) {
         if (!visited[v]) {
-            visited[v] = true;
+            visited[v]=true;
             path.push_back(v);
 
-            if (solve(v, count + 1))
+            if (solve(v,cnt+1))
                 return true;
 
-            visited[v] = false;  
+            visited[v]=false;  
             path.pop_back();
         }
     }
@@ -41,15 +41,15 @@ bool solve(int u, int count) {
 }
 
 int main() {
-    visited[0] = true;
+    visited[0]=true;
     path.push_back(0);
 
-    if (solve(0, 1)) {
-        for (int x : path)
-            cout << x << " ";
-        cout << path[0]; // closing cycle
+    if (solve(0,1)) {
+        for (int x:path)
+            cout<<x<< " ";
+        cout<<path[0];
     } else {
-        cout << "No Hamiltonian Cycle";
+        cout<<"No HamiltonianCycle";
     }
     return 0;
 }
